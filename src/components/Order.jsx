ab1acb70b98027eb345bd0d58f2ce14d2ec81186
Riley Benson <ryguybenson@yahoo.com>
2019-05-10 10:18:20 -0700

    create order component

---
+++ src/components/Order.jsx
@@ -0,0 +1,30 @@
+import React from './react';
+
+function Order() {
+  return(
+    <div stlye={order}>
+      <div style={list}>
+        <h3>1</h3>
+        <h3>First item</h3>
+      </div>
+      <div style={list}>
+        <h3>2</h3>
+        <h3>Second item</h3>
+      </div>
+      <div style={list}>
+        <h3>3</h3>
+        <h3>Third item</h3>
+      </div>
+      <div style={list}>
+        <h3>4</h3>
+        <h3>Fourth item</h3>
+      </div>
+      <div style={list}>
+        <h3>5</h3>
+        <h3>Fifth item</h3>
+      </div>
+    </div>
+  )
+}
+
+export default Order;
