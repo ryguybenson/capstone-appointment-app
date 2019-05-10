1fb8bcabddfc5f3cbb7238636ca2ae9344323009
Riley Benson <ryguybenson@yahoo.com>
2019-05-10 10:06:00 -0700

    create sign in component

---
+++ src/components/SignIn.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+function SignIn() {
+  var signIn = {
+    display: 'flex',
+    flexDirection: 'column',
+    border: '1px solid black',
+    padding: '2%'
+  }
+  var signUp = {
+    textAlign: 'center'
+  }
+  return (
+    <div>
+      <div style={signIn}
+        <input placeholder='text'/>
+        <input placeholder='text'/>
+        <button>Sign In</button>
+      </div>
+      <div style={signUp}>
+        <button>Sign Up</button
+      </div>
+    </div>
+  )
+}
+
+export default SignIn;
